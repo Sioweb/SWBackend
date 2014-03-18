7fb6045e498ced3059b42085244fdcb2d8ad040f
Sascha Weidner <siowebde@gmail.com>
2014-03-18 05:50:37 +0100

    Update config.php

--- system/modules/SWBackend/config/config.php
+++ system/modules/SWBackend/config/config.php
@@ -30,15 +30,12 @@ if(TL_MODE == 'BE')
 	$GLOBALS['TL_HOOKS']['initializeSystem'][]  = array('Backend', 'sw_initialize');
 	$GLOBALS['TL_HOOKS']['getUserNavigation'][] = array('Backend', 'changeNavigation');
 	$GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('Backend', 'extendFileTree');
-}
-
-if($_POST['action'] == 'dragNdrop')
-	$GLOBALS['TL_HOOKS']['executePostActions'][] = array('Backend','dragNdropUpload');
-
-if(TL_MODE == 'BE') 
-{
+	
 	$GLOBALS['TL_JAVASCRIPT'][] = 'assets/sioweb/sioweb.min.js?sioweb=true&amp;request_token='.$_SESSION['REQUEST_TOKEN'];
 	$GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/SWBackend/assets/sioweb.js';
 	$GLOBALS['TL_CSS'][] = 'system/modules/SWBackend/assets/sioweb.css';
 	require_once TL_ROOT . '/system/modules/SWBackend/config/icons/replacer.php';
 }
+
+if($_POST['action'] == 'dragNdrop')
+	$GLOBALS['TL_HOOKS']['executePostActions'][] = array('Backend','dragNdropUpload');
